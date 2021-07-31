6bce8e9ee43367e7a7fe86848ae560dd3a7cde7b
Mychecksdead <mert.koksal.026@gmail.com>
2021-07-31 12:29:35 +0300

    problme count by index completed

--- problem_count_by_index.py
+++ problem_count_by_index.py
@@ -25,3 +25,30 @@ def printt(r, param, cond='-1', condp='-1', s='-1', returndata=0):
 def creq(r):
   if r.status_code == 500:
     print('Connection failed...')
+
+r = rq.get(url+'contest.list')
+# for _ in r.json()['result']:
+  # print(_['id'])
+r.json()['result'][0]
+
+handle = input('Enter handle: ')
+# handle = 'mychecksdead'
+r = rq.get(url+'user.status?handle='+handle)
+creq(r)
+data = printt(r, 'index', 'OK', 'verdict', 'problem', 1)
+labels = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
+arr = np.zeros([8], dtype='uint64')
+data = np.array(data)
+data = pd.DataFrame(data)
+
+for i in range(8):
+   arr[i] = data.loc[data[0] == labels[i]].count()[0]
+
+plt.bar(labels, arr)
+plt.title('Submissions of ' + handle + ' by Indexes')
+plt.xlabel('Index')
+plt.ylabel('Count')
+plt.yticks(np.arange(0, (np.max(arr)+9)//10*10+10, 10))
+style.use('seaborn')
+
+plt.show()
